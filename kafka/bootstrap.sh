8e580a2feecbaaf5adba7a5376c0fff003bcea25
Ilyushin Evgeniy <john.ilyushin@gmail.com>
2018-01-24 19:09:24 +0300

    Fixed Kafka

---
+++ kafka/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Run in daemon mode, don't exit
+while true; do
+  sleep 100;
+done
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker rmi -f cdh-oryx
-docker build -t cdh-oryx ./
+docker build -t cdh-oryx ./cdh
+docker build -t kafka ./kafka
 docker-compose up -d
 
