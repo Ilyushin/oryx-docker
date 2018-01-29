966984592e459d2f13c0755d178bcff47190867d
Evgeniy Ilyushin <john.ilyushin@gmail.com>
2018-01-29 11:01:17 +0300

    Fixed dockerfiles

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker build -t cdh-oryx ./cdh/
 #docker stop cdh-oryx
 #docker rm cdh-oryx
 
-docker run --name cdh-oryx --hostname=quickstart.cloudera --privileged=true -t -i -p 80:80 -p 7180:7180 -p 8032:8032 -d cdh-oryx /bin/bash
+docker run --name cdh-oryx --hostname=quickstart.cloudera --privileged=true -t -i -P -d cdh-oryx /bin/bash
 
 
 
